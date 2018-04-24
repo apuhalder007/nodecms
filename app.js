2b87261289bfd23e320b14480a7c2ece8add2a65
apuplay007 <apupaly007@gmail.com>
2018-04-25 01:34:20 +0530

    added contact form validation but have to check its not working properly

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 app.set('views', __dirname+"/views/");
 app.set('view engine', "ejs");
-app.use(express.static('public'))
+app.use(express.static('public'));
 const routes = require('./routes/frontend/routes');
 app.use('/', routes);
 
--- routes/frontend/routes.js
+++ routes/frontend/routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+const { matchedData, sanitize } = require('express-validator/filter');
 router.get('/', (req, res)=>{
     res.render('frontend/index', {title: "Welcome to the brand!"});
 });
@@ -15,6 +17,44 @@ router.get('/tour', (req, res)=>{
 router.get('/contact-us', (req, res)=>{
     res.render('frontend/contact-us', {title: "Contact Us"});
 });
+
+router.post('/contact-us',
+[
+    check('email')
+    // Every validator method in the validator lib is available as a
+    // method in the check() APIs.
+    // You can customize per validator messages with .withMessage()
+    .isEmail().withMessage('must be an email')
+
+    // Every sanitizer method in the validator lib is available as well!
+    .trim()
+    .normalizeEmail(),
+
+    // ...or throw your own errors using validators created with .custom()
+    // .custom(value => {
+    //     return findUserByEmail(value).then(user => {
+    //     throw new Error('this email is already in use');
+    //     })
+    // }),
+
+    // No special validation required? Just check if data exists:
+    check('name').isEmpty().withMessage('must be fill').trim(),
+
+    // No special validation required? Just check if data exists:
+    check('comments').isEmpty().withMessage('must be fill').trim()
+
+], 
+(req, res, next)=>{
+     
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.mapped() });
+    }else{
+        res.end(JSON.stringify(req.body, null, 2));
+    }
+    
+});
+
 router.get('*', (req, res)=>{
     res.render('frontend/not-found', {title: "Not Found"});
 });
