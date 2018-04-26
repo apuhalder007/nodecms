3dc5d36dc149ed1d375399c25312da7978e260c9
apyplay007 <apuplay007@gmail.com>
2018-04-26 12:58:14 +0530

    controller added

---
+++ controller/pages.js
@@ -0,0 +1,40 @@
+let pages = {};
+
+pages.home = function(req, res){
+    res.render('frontend/index', { title: "Welcome to the brand!" });
+}
+pages.about = function (req, res) {
+    res.render('frontend/about-us', { title: "About Us" });
+}
+pages.tour = function (req, res) {
+    res.render('frontend/tour', { title: "Tour" });
+}
+pages.contact = function (req, res) {
+    res.render('frontend/contact-us', { title: "Contact Us" });
+}
+pages.contact_post = function (req, res) {
+
+    let name = req.body.name;
+    let email = req.body.email;
+    let comments = req.body.comments;
+
+    req.checkBody('name', 'Name is required').notEmpty();
+    req.checkBody('email', 'Enter a valid email').isEmail();
+
+    var errors = req.validationErrors();
+    if (errors) {
+        req.session.errors = errors;
+        req.session.success = false;
+        //res.redirect('/blockchain');
+        return res.status(422).json({ errors: errors });
+    }
+    else {
+        req.session.success = true;
+        //res.redirect('/');
+        res.end(JSON.stringify(req.body, null, 2));
+    }
+}
+pages.not_found = function(req, res){
+    res.render('frontend/not-found', { title: "Not Found" });
+}
+module.exports = pages;
--- routes/frontend/routes.js
+++ routes/frontend/routes.js
@@ -1,48 +1,17 @@
 const express = require('express');
 const router = express.Router();
-router.get('/', (req, res)=>{
-    res.render('frontend/index', {title: "Welcome to the brand!"});
-});
 
-router.get('/about-us', (req, res)=>{
-    res.render('frontend/about-us', {title: "About Us"});
-});
+const pagesController = require('../../controller/pages');
+router.get('/', pagesController.home);
 
-router.get('/tour', (req, res)=>{
-    res.render('frontend/tour', {title: "Tour"});
-});
+router.get('/about-us', pagesController.about);
 
-router.get('/contact-us', (req, res)=>{
-    res.render('frontend/contact-us', {title: "Contact Us"});
-});
+router.get('/tour', pagesController.tour);
 
-router.post('/contact-us',(req, res)=>{
+router.get('/contact-us', pagesController.contact);
 
+router.post('/contact-us', pagesController.contact_post);
 
-    let name = req.body.name;
-    let email = req.body.email;
-    let comments = req.body.comments;
-
-    req.checkBody('name', 'Name is required').notEmpty();
-    req.checkBody('email', 'Enter a valid email').isEmail();
-
-    var errors = req.validationErrors();
-    if (errors) {
-        req.session.errors = errors;
-        req.session.success = false;
-        //res.redirect('/blockchain');
-        return res.status(422).json({ errors: errors });
-    }
-    else {
-        req.session.success = true;
-        //res.redirect('/');
-        res.end(JSON.stringify(req.body, null, 2));
-    }
-    
-});
-
-router.get('*', (req, res)=>{
-    res.render('frontend/not-found', {title: "Not Found"});
-});
+router.get('*', pagesController.not_found);
 
 module.exports = router;
